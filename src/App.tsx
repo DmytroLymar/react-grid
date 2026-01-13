afc9201acbf5dcbfe6145dc01614a5ba40ac6d4f
DmytroLymar <dmytrolymar901@gmail.com>
2026-01-13 11:33:17 +0200

    Data formating section

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Grid, Table, TableHeaderRow } from '@devexpress/dx-react-grid-material-
 import { generateRows, globalSalesValues } from './demo-data/generator';
 import { useState } from 'react';
 import { TableRow } from './components/TableRow';
-import { TableCell } from './components/TableCell';
+import { CurrencyTypeProvider, DateTypeProvider } from './components/Formaters';
 
 function App() {
     const [columns] = useState([
@@ -12,13 +12,19 @@ function App() {
         { name: 'sector', title: 'Sector' },
         { name: 'customer', title: 'Customer' },
         { name: 'product', title: 'Product' },
+        { name: 'saleDate', title: 'Sale Date' },
         { name: 'amount', title: 'Sale Amount' }
     ]);
     const [rows] = useState(generateRows({ columnValues: globalSalesValues, length: 8 }));
+    const [dateColumns] = useState(['saleDate']);
+    const [currencyColumns] = useState(['amount']);
+
     return (
         <Paper>
             <Grid rows={rows} columns={columns}>
-                <Table rowComponent={TableRow} cellComponent={TableCell} />
+                <CurrencyTypeProvider for={currencyColumns} />
+                <DateTypeProvider for={dateColumns} />
+                <Table rowComponent={TableRow} />
                 <TableHeaderRow />
             </Grid>
         </Paper>
